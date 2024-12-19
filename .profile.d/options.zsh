b8dddae4be779840292b3fd145b5753db32ef187
Ridwan Faiz <rid1.fz.06@gmail.com>
2024-12-19 21:58:07 +0600

    feat(zsh): added PROMPT_SUBST shell option

--- .profile.d/options.zsh
+++ .profile.d/options.zsh
@@ -1,6 +1,6 @@
 #!/bin/env zsh
 
 # >>> Options >>>
-setopt GLOB_DOTS AUTO_CD GLOB_COMPLETE MENU_COMPLETE AUTO_LIST COMPLETE_IN_WORD
+setopt GLOB_DOTS AUTO_CD GLOB_COMPLETE MENU_COMPLETE AUTO_LIST COMPLETE_IN_WORD PROMPT_SUBST
 compinit -d "${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/zcompdump"
 # <<< Options <<<
