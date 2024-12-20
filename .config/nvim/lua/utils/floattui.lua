e6c9f853ba8dbdd6804623b2a8ab5e1ca0a4c6c2
Ridwan Faiz <rid1.fz.06@gmail.com>
2024-12-20 17:11:58 +0600

    feat(nvim): added binding to hide floating tui window using escape key

--- .config/nvim/lua/utils/floattui.lua
+++ .config/nvim/lua/utils/floattui.lua
@@ -41,20 +41,41 @@ local create_floating_window = function(opts)
     return { buf = buf, win = win }
 end
 
-M.open = function(command)
-    state.floating = create_floating_window({ buf = state.floating.buf })
-    if vim.bo[state.floating.buf].buftype ~= "terminal" then
-        vim.cmd.terminal(command)
-        vim.cmd([[startinsert]])
-    end
-
+local set_autocommands = function(bufnr)
+    -- Close floating window if process exits
     vim.api.nvim_create_autocmd("TermClose", {
-        buffer = state.floating.buf,
+        buffer = bufnr,
         group = vim.api.nvim_create_augroup("CloseTerminalBuffer", { clear = true }),
         callback = function()
             vim.cmd([[bdelete]])
         end,
     })
+end
+
+local set_keymaps = function(bufnr)
+    -- Use <Esc> to hide floating window
+    vim.keymap.set("t", "<Esc>", function()
+        vim.api.nvim_win_hide(state.floating.win)
+    end, {
+        desc = "Hide Floating TUI",
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        nowait = true,
+    })
+end
+
+M.open = function(command)
+    if not vim.api.nvim_win_is_valid(state.floating.win) then
+        state.floating = create_floating_window({ buf = state.floating.buf })
+        if vim.bo[state.floating.buf].buftype ~= "terminal" then
+            vim.cmd.terminal(command)
+        end
+        vim.cmd([[startinsert]])
+    end
+
+    set_autocommands(state.floating.buf)
+    set_keymaps(state.floating.buf)
 end
 
 return M
