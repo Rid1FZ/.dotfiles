db2523b71d0688f9ae13f8ea48be4f6d124ac4f0
Ridwan Faiz <rid1.fz.06@gmail.com>
2024-12-24 02:06:55 +0600

    fix(nvim): called correct functions in cmp-keymappings

--- .config/nvim/lua/configs/plugins/nvim-cmp.lua
+++ .config/nvim/lua/configs/plugins/nvim-cmp.lua
@@ -51,12 +51,11 @@ M.formatting = {
 }
 
 M.mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<esc>"] = cmp.mapping.close(),
+    ["<C-p>"] = cmp.select_prev_item(),
+    ["<C-n>"] = cmp.select_next_item(),
+    ["<C-d>"] = cmp.scroll_docs(-4),
+    ["<C-f>"] = cmp.scroll_docs(4),
+    ["<esc>"] = cmp.close(),
 
     ["<CR>"] = cmp.mapping(function(fallback)
         if cmp.visible() and cmp.get_active_entry() then
