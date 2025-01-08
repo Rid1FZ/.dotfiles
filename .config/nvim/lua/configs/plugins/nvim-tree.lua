b14d38e603f09da13925fc8bbd385c34665b8a52
Ridwan Faiz <rid1.fz.06@gmail.com>
2025-01-08 16:19:49 +0600

    feat(nvim): added root-folder label in NvimTree

--- .config/nvim/lua/configs/plugins/nvim-tree.lua
+++ .config/nvim/lua/configs/plugins/nvim-tree.lua
@@ -109,7 +109,7 @@ M.trash = {
 }
 
 M.renderer = {
-    root_folder_label = false,
+    root_folder_label = ":~:s?$?/...?",
     highlight_git = true,
     highlight_opened_files = "none",
     symlink_destination = false,
