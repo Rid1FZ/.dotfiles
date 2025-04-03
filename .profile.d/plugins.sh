302fdae6f0e2d87677be6ab998ee9e630792ee96
Ridwan Faiz <rid1.fz.06@gmail.com>
2025-04-03 15:28:43 +0600

    chore: changed instructions to setup conda environment

--- .profile.d/plugins.sh
+++ .profile.d/plugins.sh
@@ -3,15 +3,14 @@ source <(fzf --bash)
 # <<< fzf integration <<<
 
 # >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$("${HOME}/.mamba/bin/conda" 'shell.bash' 'hook' 2>/dev/null)"
+__conda_setup="$("${HOME}/anaconda3/bin/conda" 'shell.bash' 'hook' 2>/dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "${HOME}/.mamba/etc/profile.d/conda.sh" ]; then
-        source "${HOME}/.mamba/etc/profile.d/conda.sh"
+    if [ -f "${HOME}/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "${HOME}/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="${HOME}/.mamba/bin:$PATH"
+        export PATH="${HOME}/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
--- .zprofile.d/plugins.zsh
+++ .zprofile.d/plugins.zsh
@@ -5,22 +5,17 @@ source <(fzf --zsh)
 # <<< fzf integration <<<
 
 # >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$("${HOME}/.mamba/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$("${HOME}/anaconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "${HOME}/.mamba/etc/profile.d/conda.sh" ]; then
-        source "${HOME}/.mamba/etc/profile.d/conda.sh"
+    if [ -f "${HOME}/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "${HOME}/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="${HOME}/.mamba/bin:$PATH"
+        export PATH="${HOME}/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
-
-if [ -f "${HOME}/.mamba/etc/profile.d/mamba.sh" ]; then
-    source "${HOME}/.mamba/etc/profile.d/mamba.sh"
-fi
 # <<< conda initialize <<<
 
 # >>> source plugins from plugins directory >>>
