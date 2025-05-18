a191e7fa6cdbdf70d342974201b4a9d9b3e75439
Ridwan Faiz <rid1.fz.06@gmail.com>
2025-05-19 05:58:12 +0600

    feat(nvim): use config file in project for black config instead of hard coding config option

--- .config/nvim/lua/configs/plugins/none-ls.lua
+++ .config/nvim/lua/configs/plugins/none-ls.lua
@@ -3,9 +3,7 @@ local null_ls = require("null-ls")
 
 M.sources = {
     -- python
-    null_ls.builtins.formatting.black.with({
-        extra_args = { "--line-length", "120" },
-    }),
+    null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
 
     -- lua
