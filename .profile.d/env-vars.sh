79c5a931be1dc67380def557762effbc849db3f5
Ridwan Faiz <rid1.fz.06@gmail.com>
2025-06-20 19:58:41 +0600

    feat(bash+zsh): added check before setting environment variable

--- .profile.d/env-vars.sh
+++ .profile.d/env-vars.sh
@@ -1,32 +1,58 @@
 #!/bin/env bash
 
+function set_env {
+    local name=$1
+    local value=$2
+    local -a reserved=(
+        PATH
+        HOME
+        SHELL
+        USER
+        PWD
+        OLDPWD
+        SHLVL
+        LD_LIBRARY_PATH
+    )
+
+    for r in "${reserved[@]}"; do
+        if [[ "${name}" == "${r}" ]]; then
+            return 0
+        fi
+    done
+
+    export "$name"="$value"
+    return 0
+}
+
 # >>> environment variables >>>
-export PATH="${HOME}/.cargo/bin:${HOME}/.local/bin:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
-export COLORTERM="truecolor"
-export DOTFILES="${HOME}/.dotfiles"
-export VISUAL="nvim"
-export EDITOR="${VISUAL}"
-export SUDO_EDITOR="${VISUAL}"
-export JAVA_HOME="/usr/java/latest"
-export MAMBA_NO_BANNER=1
-export MANROFFOPT="-c"
-export PAGER="bat --plain"
-export MANPAGER="nvimpager"
-export BAT_PAGER=""
-export RIPGREP_CONFIG_PATH="${XDG_CONFIG_HOME:-${HOME}/.config}/ripgrep/ripgreprc"
-export HISTSIZE=100000
-export SAVEHIST=100000
-export FZF_DEFAULT_OPTS_FILE="${XDG_CONFIG_HOME:-${HOME}/.config}/fzf/fzfrc"
+set_env PATH "${HOME}/.cargo/bin:${HOME}/.local/bin:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
+set_env COLORTERM "truecolor"
+set_env DOTFILES "${HOME}/.dotfiles"
+set_env VISUAL "nvim"
+set_env EDITOR "${VISUAL}"
+set_env SUDO_EDITOR "${VISUAL}"
+set_env JAVA_HOME "/usr/java/latest"
+set_env MAMBA_NO_BANNER "1"
+set_env MANROFFOPT "-c"
+set_env PAGER "bat --plain"
+set_env MANPAGER "nvimpager"
+set_env BAT_PAGER ""
+set_env RIPGREP_CONFIG_PATH "${XDG_CONFIG_HOME:-${HOME}/.config}/ripgrep/ripgreprc"
+set_env HISTSIZE "100000"
+set_env SAVEHIST "100000"
+set_env FZF_DEFAULT_OPTS_FILE "${XDG_CONFIG_HOME:-${HOME}/.config}/fzf/fzfrc"
 
 case "$(ps -p $$ -o comm=)" in
     "zsh")
-        export HISTFILE="${XDG_STATE_HOME:-${HOME}/.local/state}/zsh/zsh_history"
+        set_env HISTFILE "${XDG_STATE_HOME:-${HOME}/.local/state}/zsh/zsh_history"
         fpath+=("${XDG_DATA_HOME:-${HOME}/.local/share}/zsh/completions")
         export fpath
         ;;
     "bash")
-        export HISTFILE="${XDG_STATE_HOME:-${HOME}/.local/state}/bash/bash_history"
-        export HISTCONTROL="ignoreboth"
+        set_env HISTFILE "${XDG_STATE_HOME:-${HOME}/.local/state}/bash/bash_history"
+        set_env HISTCONTROL "ignoreboth"
         ;;
 esac
+
+unset set_env
 # <<< environment variables <<<
