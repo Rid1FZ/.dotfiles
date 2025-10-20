1a8c2d0ac233923a7ae517403fe93b45d75e5852
Ridwan Faiz <rid1.fz.06@gmail.com>
2025-10-20 21:14:01 +0600

    fix(nvim): forcefully redraw statusline when mode is changed

--- .config/nvim/lua/utils/statusline/autocommands.lua
+++ .config/nvim/lua/utils/statusline/autocommands.lua
@@ -3,11 +3,20 @@ local M = {}
 function M.setup_autocommands()
     local statusline_group = vim.api.nvim_create_augroup("Statusline", { clear = true })
 
-    vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter", "ModeChanged", "DiagnosticChanged" }, {
+    vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
         pattern = "*",
         group = statusline_group,
         callback = function()
             vim.opt_local.statusline = "%!v:lua.Statusline.active()"
+            vim.cmd.redrawstatus()
+        end,
+    })
+
+    vim.api.nvim_create_autocmd({ "ModeChanged", "DiagnosticChanged" }, {
+        pattern = "*",
+        group = statusline_group,
+        callback = function()
+            vim.cmd.redrawstatus()
         end,
     })
 
