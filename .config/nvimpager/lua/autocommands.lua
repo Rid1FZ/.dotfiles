7502366b3cfea9b646ab3a9611d8a5717ca17656
Ridwan Faiz <rid1.fz.06@gmail.com>
2025-11-06 15:56:06 +0600

    feat(pager): changed default pager to `less` from `nvimpager`

--- .config/nvimpager/lua/autocommands.lua
+++
@@ -1,7 +0,0 @@
--- Highlight Yanked Part
-vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-})
