75f866d288ab13e787e67221a72a295002df745f
Ridwan Faiz <rid1.fz.06@gmail.com>
2026-01-31 13:08:03 +0600

    feat(nvim): automatically hide branch name if the window size is too small

--- .config/nvim/lua/utils/statusline/components/git.lua
+++ .config/nvim/lua/utils/statusline/components/git.lua
@@ -1,5 +1,8 @@
 local M = {}
 
+local fn = vim.fn
+local api = vim.api
+
 local last_branch = ""
 local last_cwd = ""
 
@@ -8,13 +11,13 @@ local function get_branch()
         return ""
     end
 
-    local cwd = vim.fn.getcwd()
+    local cwd = fn.getcwd()
 
     if cwd == last_cwd and last_branch ~= "" then
         return last_branch
     end
 
-    local branch = vim.fn.system("git branch --show-current 2>/dev/null | tr -d '\n'")
+    local branch = fn.system("git branch --show-current 2>/dev/null | tr -d '\n'")
     if not branch or branch == "" or branch == "HEAD" then
         branch = ""
     end
@@ -27,12 +30,15 @@ end
 
 M.get_gitbranch = function()
     local branch = get_branch()
+    local win_width = api.nvim_win_get_width(0)
 
-    if branch ~= "" then
-        return string.format("%%#StatusLineGitBranch# %s%%#StatusLine#  ", branch)
-    else
+    if branch == "" then
         return " "
+    elseif (#branch + 2) > math.floor(win_width * 0.15) then
+        return string.format("%%#StatusLineGitBranch#%%#StatusLine#  ")
     end
+
+    return string.format("%%#StatusLineGitBranch# %s%%#StatusLine#  ", branch)
 end
 
 return M
