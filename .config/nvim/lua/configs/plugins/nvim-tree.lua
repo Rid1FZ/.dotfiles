2fd3a62324b1b0f838dd65abfc1e3776da4603c4
Ridwan Faiz <rid1.fz.06@gmail.com>
2026-02-01 08:15:48 +0600

    feat(nvim): removed highlighting for git changes in `NvimTree`

--- .config/nvim/lua/configs/plugins/nvim-tree.lua
+++ .config/nvim/lua/configs/plugins/nvim-tree.lua
@@ -115,7 +115,7 @@ M.trash = {
 
 M.renderer = {
     root_folder_label = false,
-    highlight_git = true,
+    highlight_git = false,
     highlight_opened_files = "none",
     symlink_destination = false,
 
