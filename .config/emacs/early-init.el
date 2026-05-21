dadc21e3171128f9c6bb3d26b8f681579c30f9ac
Ridwan Faiz <rid1.fz.06@gmail.com>
2026-05-21 08:36:32 +0600

    feat(emacs): moved some files and directories to `~/.cache/emacs` and `~/.local/share/emacs`

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -6,24 +6,44 @@
 
 ;; Emacs checks every file load against file-name-handler-alist.
 ;; Disable this during startup for performance.
-(defvar my/file-name-handler-alist file-name-handler-alist)
+(defvar user/file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq file-name-handler-alist my/file-name-handler-alist)))
+            (setq file-name-handler-alist user/file-name-handler-alist)))
+
+(require 'xdg)
+
+(defvar user/emacs-cache-dir
+  (expand-file-name "emacs/" (xdg-cache-home))
+  "Emacs cache directory. Defaults to ~/.cache/emacs/.")
+
+(defvar user/emacs-local-dir
+  (expand-file-name "emacs/" (xdg-data-home))
+  "Emacs local/data directory. Defaults to ~/.local/share/emacs/.")
+
+;; Ensure directories exist early so downstream code never fails on mkdir.
+(dolist (dir (list user/emacs-cache-dir user/emacs-local-dir))
+  (unless (file-directory-p dir)
+    (make-directory dir :parents)))
+
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache
+   (expand-file-name "eln-cache/" user/emacs-cache-dir)))
 
 (setq disabled-command-function nil
       make-backup-files nil
       create-lockfiles nil
       evil-want-keybinding nil
       read-process-output-max (* 1024 1024) ;; set read buffer to 1MiB for lesser system calls
-      package-quickstart t
-      native-comp-async-jobs-number (num-processors)
+      native-comp-async-jobs-number (/ (num-processors) 2)
       native-comp-async-report-warnings-errors 'silent
       byte-compile-warnings '(not obsolete)
       warning-minimum-log-level :warning
-      package-user-dir (expand-file-name "packages/" user-emacs-directory))
+      package-user-dir  (expand-file-name "packages/" user/emacs-local-dir)
+      package-quickstart t
+      package-quickstart-file (expand-file-name "package-quickstart.el" user/emacs-local-dir))
 
 ;; Disable Autosave for Programming Modes
 (add-hook 'prog-mode-hook (lambda ()
