e093d0981ab80bc1f4bd853d6f003620051581fa
Ridwan Faiz <rid1.fz.06@gmail.com>
2026-05-25 22:39:50 +0600

    feat: removed `nixos` config and scripts and scripts for hyprland

--- nixos/modules/boot.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.consoleMode = "2";
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-}
-
